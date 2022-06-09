e643a9ec2806003309d47bfc642a1f25effd8ded
riapacheco <me@riapacheco.com>
2022-06-08 19:07:17 -0600

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ import { YutesFileComponent } from './views/yutes-file/yutes-file.component';
 const routes: Routes = [
   {
     path: '',
-    component: ButtonsComponent
+    component: ButtonsComponent,
   },
   {
     path: 'home',
